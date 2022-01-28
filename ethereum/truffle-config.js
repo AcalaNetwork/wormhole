bee413e1c4284ad4d2ceaff8c0cc345bc3f26ef3
Shunji Zhan <shunhahaha@live.com>
2022-01-28 10:43:49 +0800

    temp POC works

--- ethereum/truffle-config.js
+++ ethereum/truffle-config.js
@@ -1,11 +1,22 @@
 require("dotenv").config({ path: ".env" });
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
+const mnemonicPhrase = 'myth like bonus scare over problem client lizard pioneer submit female collect';  // fake eth mandala
+
 module.exports = {
   networks: {
     development: {
+      provider: () =>
+        new HDWalletProvider(mnemonicPhrase, 'http://localhost:8545'),
       host: "127.0.0.1",
       port: 8545,
+      network_id: 595,
+      gasPrice: 200786445289, // storage_limit = 64001, validUntil = 360001, gasLimit = 10000000
+      gas: 42032000,
+    },
+    development2: {
+      host: "127.0.0.1",
+      port: 8546,
       network_id: "*",
     },
     mainnet: {
